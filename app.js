9350ee4b5b464aa62102fc08f223c775fdb5dadf
Charles Afolabi <charles49ja@gmail.com>
2025-10-07 16:13:38 +0100

    cors updated

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
 app.use(express.json())
 app.use(cookieParser())
 
